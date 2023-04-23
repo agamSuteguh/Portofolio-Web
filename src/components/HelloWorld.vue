4929270589438750c1e85e406af46324840ac2fe
Agam <agaupper@gmail.com>
2023-04-23 23:39:04 +0700

    update front end

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,74 +1,74 @@
 <template>
 
-    <div class="bg-gry p-16">
-      <div class="p-8 shadow mt-24">
-        <div class="grid grid-cols-1 md:grid-cols-3">
-          <div
-            class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0"
-          >
-           <a href="https://github.com/agamsuteguh"> <div>
-            <svg class="ml-14 bg-navy" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-  <path fill="#FFF" d="M0 0v24h24v-24h-24zm14.534 19.59c-.406.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.311-1.588-.824-2.147.083-.202.357-1.016-.079-2.117 0 0-.671-.215-2.198.82-.639-.18-1.323-.267-2.003-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/>
+  <div class="bg-gry p-16">
+    <div class="p-8 shadow mt-24">
+      <div class="grid grid-cols-1 md:grid-cols-3">
+        <div
+          class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0"
+        >
+         <a href="https://github.com/agamsuteguh"> <div>
+          <svg class="ml-14 bg-navy" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+<path fill="#FFF" d="M0 0v24h24v-24h-24zm14.534 19.59c-.406.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.311-1.588-.824-2.147.083-.202.357-1.016-.079-2.117 0 0-.671-.215-2.198.82-.639-.18-1.323-.267-2.003-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/>
 </svg>
 
-              <p  class="text-beige ml-1">Github</p>
-            </div></a>
-            <a  href="https://www.instagram.com/ag32111/"> <div>
-            <img class="ml-12" src="https://www.svgrepo.com/show/343562/instagram-social-media-network-communication-interaction-connection.svg" width="25px" alt="">
-              <p  class="text-beige ml-1">Instagram</p>
-            </div></a>
-       
-           <a  href="https://www.linkedin.com/in/agam-suteguh/"> <div>
-            <img class="ml-12" src="https://www.svgrepo.com/show/448234/linkedin.svg" width="25px" alt="">
-              <p  class="text-beige ml-1">LinkedIn</p>
-            </div></a>
-          </div>
-          <div class="relative">
-            <div
-              class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500"
-            >
-             <img class="" width="150px" src="https://i.ibb.co/kQQwLQ7/IMG-20230423-213620-405-removebg-preview-removebg-preview.png" alt="Screenshot-2023-04-23-01-16-44-29-removebg-preview" border="0">
-              
-            
-            </div>
-          </div>
+            <p  class="text-beige ml-1">Github</p>
+          </div></a>
+          <a  href="https://www.instagram.com/ag32111/"> <div>
+          <img class="ml-12" src="https://www.svgrepo.com/show/343562/instagram-social-media-network-communication-interaction-connection.svg" width="25px" alt="">
+            <p  class="text-beige ml-1">Instagram</p>
+          </div></a>
+     
+         <a  href="https://www.linkedin.com/in/agam-suteguh/"> <div>
+          <img class="ml-12" src="https://www.svgrepo.com/show/448234/linkedin.svg" width="25px" alt="">
+            <p  class="text-beige ml-1">LinkedIn</p>
+          </div></a>
+        </div>
+        <div class="relative">
           <div
-            class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center"
+            class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500"
           >
-            <button
-              class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
-            >
-              Connect
-            </button>
-            <button
-              class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
-            >
-              Message
-            </button>
+           <img class="" width="150px" src="https://i.ibb.co/kQQwLQ7/IMG-20230423-213620-405-removebg-preview-removebg-preview.png" alt="Screenshot-2023-04-23-01-16-44-29-removebg-preview" border="0">
+            
+          
           </div>
         </div>
-        <div class="mt-20 text-center border-b pb-12">
-          <h1 class="text-4xl font-medium text-gray-700">
-            Agam Suteguh <span class="font-light text-gray-500"></span>
-          </h1>
-          <p class="font-light text-gray-600 mt-3">Klapanunggal, Bogor</p>
-          <p class="mt-8 text-gray-500">
-            Web Developer
-          </p>
-
-        </div>
-        <div class="mt-12 flex flex-col justify-center">
-          <p class="text-gray-600 text-center font-light lg:px-16">
-            An artist of considerable range, Ryan — the name taken by
-            Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
-            records all of his own music, giving it a warm, intimate feel with a
-            solid groove structure. An artist of considerable range.
-          </p>
-          <button class="text-indigo-500 py-2 px-4 font-medium mt-4">
-            Show more
+        <div
+          class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center"
+        >
+          <button
+            class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+          >
+            Connect
+          </button>
+          <button
+            class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+          >
+            Message
           </button>
         </div>
       </div>
+      <div class="mt-20 text-center border-b pb-12">
+        <h1 class="text-4xl font-medium text-gray-700">
+          Agam Suteguh <span class="font-light text-gray-500"></span>
+        </h1>
+        <p class="font-light text-gray-600 mt-3">Klapanunggal, Bogor</p>
+        <p class="mt-8 text-gray-500">
+          Web Developer
+        </p>
+
+      </div>
+      <div class="mt-12 flex flex-col justify-center">
+        <p class="text-gray-600 text-center font-light lg:px-16">
+          An artist of considerable range, Ryan — the name taken by
+          Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
+          records all of his own music, giving it a warm, intimate feel with a
+          solid groove structure. An artist of considerable range.
+        </p>
+        <button class="text-indigo-500 py-2 px-4 font-medium mt-4">
+          Show more
+        </button>
+      </div>
     </div>
+  </div>
 
 </template>
