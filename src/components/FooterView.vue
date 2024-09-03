97b4c1c16fa25d3b3090c72c1c820f937e5c4894
Agam Suteguh <100204292+agamSuteguh@users.noreply.github.com>
2024-09-03 16:58:15 +0700

    Update FooterView.vue

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -2,7 +2,7 @@
   <div class="">
     <footer class="bg-blue-400 text-center mt-20">
       <div class="p-2 text-center text-neutral-200">
-        © 2023 Copyright:
+        © 2024 Copyright:
         <a class="text-neutral-100" href="https://github.com/agamsuteguh"
           >Agam Suteguh</a
         >
